72b36a3c12dbe2d9f0422127d849298e32794094
DAI KOI YIM 2001062E <2001062E@student.tp.edu.sg>
2021-12-18 22:52:11 +0800

    Corrected Landing Page Code

---
+++ JobApplicationWebsite_Project/Client/Configuration/Entities/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplicationWebsite_Project.Client.Configuration.Entities
+{
+    public class Class
+    {
+    }
+}
