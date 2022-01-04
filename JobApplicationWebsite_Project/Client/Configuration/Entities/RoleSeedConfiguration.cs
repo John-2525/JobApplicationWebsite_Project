c9e7c6aa7c042215c17938ca3bf102867952512d
DAI KOI YIM 2001062E <2001062E@student.tp.edu.sg>
2022-01-04 17:10:27 +0800

    Created Empty RoleSeed

--- JobApplicationWebsite_Project/Client/Configuration/Entities/RoleSeedConfiguration.cs
+++ JobApplicationWebsite_Project/Client/Configuration/Entities/RoleSeedConfiguration.cs
@@ -5,8 +5,7 @@ using System.Threading.Tasks;
 
 namespace JobApplicationWebsite_Project.Client.Configuration.Entities
 {
-    public class Clas333gghelpmeplss
+    public class RoleSeedConfiguration
     {
-
     }
 }
