c3ff33ce074c283d60af87cfb39017498ee06dfd
DAI KOI YIM 2001062E <2001062E@student.tp.edu.sg>
2022-02-09 00:47:41 +0800

    Company  Form validation done

--- JobApplicationWebsite_Project/Shared/Domain/company.cs
+++ JobApplicationWebsite_Project/Shared/Domain/company.cs
@@ -1,6 +1,7 @@
 ﻿using CarRentalManagement.Shared;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,10 @@ namespace JobApplicationWebsite_Project.Shared.Domain
 {
     public class Company:BaseDomainModel
     {
-       
-        
 
+        [Required]
         public string CompanyName { get; set; }
-
+        [Required]
         public string Description { get; set; }
 
         public int? PostingID { get; set; }
